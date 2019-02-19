bf70a4b027dd9b87a91985d6f116193a884a0d14
karagudisuma <ksumaudupa@gmail.com>
2019-02-19 19:08:28 +0530

    Adding error message

--- js/eventHandler.js
+++ js/eventHandler.js
@@ -1,5 +1,54 @@
 let tableHeaderId = document.getElementById('table-header');
 let tableBodyId = document.getElementById('table-body');
+let numRowsInTable = document.getElementById('numRowsInTable');
+let totalRowsInArr = document.getElementById('totalRowsInArr');
+let indexRowInTable = document.getElementById('indexRowInTable');
+let reportDataLength = reportData.data.length;
+
+indexRowInTable.addEventListener("keydown", (event) => {
+    //On enter press
+    if(event.keyCode === 13){
+        let numOfRows = event.target.value;
+        let validRange = isAllowedLen(numOfRows, 1000000);
+        let validLen = withinArrayLen(numOfRows, reportDataLength);
+        if(validLen && validRange){
+
+        }
+        else{
+            indexRowInTable.value = 1;
+        }
+    }
+});
+
+numRowsInTable.addEventListener("keydown", (event) => {
+    //On enter press
+    if(event.keyCode === 13){
+        let numOfRows = event.target.value;
+        let validRange = isAllowedLen(numOfRows, 1000);
+        let validLen = withinArrayLen(numOfRows, reportDataLength);
+        if(validLen && validRange){
+            
+        }
+        else{
+            numRowsInTable.value = 10;
+        }
+    }
+});
+
+totalRowsInArr.addEventListener("keydown", event => {
+    //On enter press
+    if(event.keyCode === 13){
+        let numOfRows = event.target.value;
+        let validRange = isAllowedLen(numOfRows, 1000000);
+        let validLen = withinArrayLen(numOfRows, reportDataLength);
+        if(validLen && validRange){
+            
+        }
+        else{
+            totalRowsInArr.value = reportDataLength;
+        }
+    }
+});
 
 function renderTableHeader(){
     let cols = reportData.cols;
@@ -17,7 +66,7 @@ function renderTableBody(){
     let i;
     let data = reportData.data;
     for(i = 0; i < 10; i++){
-        tBody += `<tr>
+        tBody += `<tr data-row-index=${i}>
                     <td class=" pr3 bb b--black-20 pl2">${data[i][0]}</td>
                     <td class=" pr3 bl bb b--black-20 pl2">${data[i][1]}</td>
                     <td class=" pr3 bl bb b--black-20 pl2">${data[i][2]}</td>
@@ -26,6 +75,26 @@ function renderTableBody(){
                 </tr>`;
     }
     tableBodyId.innerHTML = tBody
+}
+
+function showError(msg){
+    alert(msg);
+}
+
+function withinArrayLen(inputSize, maxSize){
+    if (inputSize > maxSize){
+        showError('Number is exceeding the input data size');
+        return false;
+    }
+    return true;
+}
+
+function isAllowedLen(inputSize, maxLen){
+    if(inputSize > maxLen){
+        showError('Number is exceeding the allowed limit');
+        return false;
+    }
+    return true;
 }
 
 renderTableHeader();
